7a49fa3f8b67010cec844f4957953f7d8a50c6bf
gustavomacielp432 <gustavomacielp432@gmail.com>
2020-06-27 18:53:07 -0300

    locais visitados

--- src/components/Help.vue
+++ src/components/Help.vue
@@ -50,9 +50,6 @@ export default {
       ],
     };
   },
-  mounted() {
-    console.log(this.perguntas.cadastro);
-  },
 };
 </script>
 
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,8 +5,9 @@
     </v-card-title>
     <v-card-text>
       <v-form>
-        <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+        <v-text-field label="Username" prepend-icon="mdi-account-circle" v-model="usuario"/>
         <v-text-field
+          v-model="senha"
           :type="showPassword ? 'text' : 'password'"
           label="Password"
           prepend-icon="mdi-lock"
@@ -23,14 +24,42 @@
   </v-card>
 </template>
 <script>
+import LocaisVisitados from "./models/LocaisVisitados"
+import { baseApiUrl } from "@/global";
+import axios from "axios";
 export default {
   data () {
     return {
-      showPassword: false
+      showPassword: false,
+      usuario: "",
+      senha: ""
     }
   },
   methods: {
-    login(){
+    async geolocate() {
+      return await this.$getLocation()
+      .then(async coordinates => {
+        return await this.$http
+        .get(
+          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coordinates.lat},${coordinates.lng}&key=AIzaSyDRYA4kZPf8A9E5E-_Oj7csLiRmppBRSV8&language=pt-BR`
+        )
+        .then((response) => {
+          return response.body.results[0];
+        });
+      });
+    },
+    async login(){
+      let localizacao = await this.geolocate();
+      let localVisitado =  new LocaisVisitados();
+      localVisitado.user_id = 1
+      localVisitado.lat =  localizacao.geometry.location.lat
+      localVisitado.lng = localizacao.geometry.location.lng
+      localVisitado.cep = localizacao.address_components[localizacao.address_components.length-1].long_name
+      localVisitado.cep = localVisitado.cep.replace('-', '')
+      axios.post(`${baseApiUrl}/localVisitado`, localVisitado)
+        .then((res) => {
+          console.log(res)
+        });
       this.$router.push({ name: 'maps' })
     }
   }
