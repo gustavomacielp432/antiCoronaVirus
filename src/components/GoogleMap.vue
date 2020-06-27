5272f495cf840fdaf647a55fb0e7f75ee332e20a
gustavomacielp432 <gustavomacielp432@gmail.com>
2020-06-27 19:17:16 -0300

    getHospital

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,9 +1,9 @@
 <template>
   <v-card>
-    <!-- <div>
-      <h2>Search and add a pin</h2>
+    <div>
+      <h2>Buscar hospital mais próximo</h2>
       <label>
-        <vue-google-autocomplete
+        <!-- <vue-google-autocomplete
           style="width:100%"
           id="map"
           classname="form-control"
@@ -11,11 +11,11 @@
           v-model="currentAddress"
           v-on:placechanged="setAddress"
         >
-        </vue-google-autocomplete>
-        <button @click="addMarker">Add</button>
+        </vue-google-autocomplete> -->
+        <v-btn color="primary" @click="goToHospital">Buscar</v-btn>
       </label>
       <br />
-    </div> -->
+    </div>
     <br />
     <div class="col-sm-12 col-lg-8 mt-2 px-2">
       <v-card min-height="439px" class="blue h-100">
@@ -36,7 +36,7 @@
 </template>
 <script>
 import GoogleMaps from "./GoogleMaps";
-// import VueGoogleAutocomplete from "vue-google-autocomplete";  
+// import VueGoogleAutocomplete from "vue-google-autocomplete";
 export default {
   components: {
     GoogleMaps,
@@ -47,7 +47,7 @@ export default {
     destination: "",
     origin: "",
     showRoute: false,
-    currentPlace: null,
+    // currentPlace: null,
     places: [],
     markers: [],
     currentAddress: "",
@@ -58,41 +58,50 @@ export default {
   },
 
   methods: {
-    setAddress(addressData, placeResultData, id) {
-      console.log(addressData);
-      this.currentPlace = placeResultData;
-      console.log(id);
-    },
-    addMarker() {
-      if (this.currentPlace) {
-        this.destination = this.currentPlace;
+    // setAddress(addressData, placeResultData, id) {
+    //   console.log(addressData);
+    //   this.currentPlace = placeResultData;
+    //   console.log(id);
+    // },
+    async goToHospital() {
+      // if (this.currentPlace) {
+      //   this.destination = this.currentPlace;
+      //   this.showRoute = true;
+      //   this.currentPlace = null;
+      //   this.currentAddress = "";
+      // }
+      this.destination = await this.$getLocation().then(async (coordinates) => {
+        return await this.$http
+          .get(
+            `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyDRYA4kZPf8A9E5E-_Oj7csLiRmppBRSV8&language=pt-BR&input=hospital&inputtype=textquery&locationbias=point:${coordinates.lat},${coordinates.lng}&fields=photos,formatted_address,name,rating,opening_hours,geometry`
+          )
+          .then((response) => {
+            return response.candidates[0];
+          });
+      });
+      if(this.destination)
         this.showRoute = true;
-        this.currentPlace = null;
-        this.currentAddress = "";
-      }
     },
     geolocate: function() {
-      this.$getLocation()
-      .then(async coordinates => {
+      this.$getLocation().then(async (coordinates) => {
         this.origin = await this.$http
-        .get(
-          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coordinates.lat},${coordinates.lng}&key=AIzaSyDRYA4kZPf8A9E5E-_Oj7csLiRmppBRSV8&language=pt-BR`
-        )
-        .then((response) => {
-          return response.body.results[0];
-        });
+          .get(
+            `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coordinates.lat},${coordinates.lng}&key=AIzaSyDRYA4kZPf8A9E5E-_Oj7csLiRmppBRSV8&language=pt-BR`
+          )
+          .then((response) => {
+            return response.body.results[0];
+          });
       });
     },
     listaUsario: function() {
-      this.$getLocation()
-      .then(async coordinates => {
+      this.$getLocation().then(async (coordinates) => {
         this.origin = await this.$http
-        .get(
-          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coordinates.lat},${coordinates.lng}&key=AIzaSyDRYA4kZPf8A9E5E-_Oj7csLiRmppBRSV8&language=pt-BR`
-        )
-        .then((response) => {
-          return response.body.results[0];
-        });
+          .get(
+            `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coordinates.lat},${coordinates.lng}&key=AIzaSyDRYA4kZPf8A9E5E-_Oj7csLiRmppBRSV8&language=pt-BR`
+          )
+          .then((response) => {
+            return response.body.results[0];
+          });
       });
     },
   },
